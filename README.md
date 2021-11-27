1a900dc6b69a032b030efde39f1e6724ad6ed6be
GiDaeHyeon <81670897+GiDaeHyeon@users.noreply.github.com>
2021-11-27 19:39:31 +0900

    docker setting

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dh_setting
 DH's system setting &amp;&amp; useful things
+
+인스턴스 만들 때마다 이런저런 명령어들 입력하고,, 갑자기 기억안나서 구글링하고,, 망할 AWS Linux 사용할 때랑 ubuntu 사용할 때랑 너무 헷갈리고,,
+
+### 그래서 해당 명령어들을 한 데 모아 레포지토리에서 관리해야즹~
+
+Docker, Docker-Compose, nvidia-docker, cuda, cudnn, NGINX, python3, java, node 등등 설치를 명령어 한 줄로 끝내기!!
---
+++ install_docker.sh
@@ -0,0 +1,21 @@
+# Docker는 몇 가지 패키지에 의존하는데 아래 4개의 패키지를 설치해준다.
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Docker 패키지 저장소를 apt에 등록한다.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+
+# Docker를 설치하고, 실행한다.
+sudo apt install -y docker-ce
+sudo systemctl start docker
+
+# docker-compose를 설치한다.
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# 알맞은 권한 설정과 심볼릭 링크를 설정해준다.
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose -version
+
